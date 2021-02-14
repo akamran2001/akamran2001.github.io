d8ccf9df014549c2a4074a73e752774714289cd6
Ahmed Muyen Kamran <akamran@colgate.edu>
2021-02-14 14:16:05 -0500

    deleted some files

--- assets/js/image.js
+++ assets/js/image.js
@@ -1,3 +1,3 @@
 //Add daily scenic image to banner
 const banner = document.getElementById("banner");
-banner.style.backgroundImage = `url(https://source.unsplash.com/${banner.offsetWidth}x${banner.offsetHeight}/daily?scenic)`;
+banner.style.backgroundImage = `url(https://source.unsplash.com/${window.innerWidth}x${window.innerHeight}/daily?scenic)`;
