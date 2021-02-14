e47b9673854b0935086f7f2d444a0e7211dee3da
Ahmed Muyen Kamran <akamran@colgate.edu>
2021-02-14 02:24:39 -0500

    image js

---
+++ assets/js/image.js
@@ -0,0 +1,4 @@
+//Add daily scenic image to banner
+document.getElementById(
+  "banner"
+).style.backgroundImage = `url(https://source.unsplash.com/${window.innerWidth}x${window.innerHeight}/daily?scenic)`;
