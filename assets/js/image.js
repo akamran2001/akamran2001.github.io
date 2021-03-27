911d31c848f5c196ba4373586c1230b5bade8e6d
Ahmed Muyen Kamran <akamran@colgate.edu>
2021-03-27 15:42:14 -0400

    changed images, font size and bg color

--- assets/js/image.js
+++ assets/js/image.js
@@ -1,3 +1,18 @@
 //Add daily scenic image to banner
 const banner = document.getElementById("banner");
 banner.style.backgroundImage = `url(https://source.unsplash.com/${window.innerWidth}x${window.innerHeight}/daily?scenic)`;
+
+const isyllabi_img = document.getElementById("isyllabi_img");
+isyllabi_img.src = `https://source.unsplash.com/NIpx9ZUSiZg/${window.innerWidth}x${window.innerHeight}`;
+
+const clock_img = document.getElementById("clock_img");
+clock_img.src = `https://source.unsplash.com/JsTmUnHdVYQ/${window.innerWidth}x${window.innerHeight}`;
+
+const saiyancity_img = document.getElementById("saiyancity_img");
+saiyancity_img.src = `https://source.unsplash.com/-98jVaVuGv0/${window.innerWidth}x${window.innerHeight}`;
+
+const bizztrak_img = document.getElementById("bizztrak_img");
+bizztrak_img.src = `https://source.unsplash.com/R401qwThw7w/${window.innerWidth}x${window.innerHeight}`;
+
+const party_img = document.getElementById("party_img");
+party_img.src = `https://source.unsplash.com/48elRjHbXRY/${window.innerWidth}x${window.innerHeight}`;
