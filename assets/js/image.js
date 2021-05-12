7290b3c412ab0a5498938caf476b9e5e41c27dbf
Ahmed Muyen Kamran <akamran@colgate.edu>
2021-05-12 00:39:20 -0400

    desktop_grid

--- assets/js/image.js
+++ assets/js/image.js
@@ -2,17 +2,22 @@
 const banner = document.getElementById("banner");
 banner.style.backgroundImage = `url(https://source.unsplash.com/${window.innerWidth}x${window.innerHeight}/daily?nature)`;
 
-const isyllabi_img = document.getElementById("isyllabi_img");
-isyllabi_img.src = `https://source.unsplash.com/NIpx9ZUSiZg/${window.innerWidth}x${window.innerHeight}`;
+// document.getElementById(
+//   "isyllabi"
+// ).style.backgroundImage = `url("https://source.unsplash.com/random/${self.offsetWidth}x${self.offsetHeight}")`;
 
-const clock_img = document.getElementById("clock_img");
-clock_img.src = `https://source.unsplash.com/JsTmUnHdVYQ/${window.innerWidth}x${window.innerHeight}`;
+// document.getElementById(
+//   "clock"
+// ).style.backgroundImage = `url("https://source.unsplash.com/random/${self.offsetWidth}x${self.offsetHeight}")`;
 
-const saiyancity_img = document.getElementById("saiyancity_img");
-saiyancity_img.src = `https://source.unsplash.com/-98jVaVuGv0/${window.innerWidth}x${window.innerHeight}`;
+// document.getElementById(
+//   "saiyancity"
+// ).style.backgroundImage = `url("https://source.unsplash.com/random/${self.offsetWidth}x${self.offsetHeight}")`;
 
-const bizztrak_img = document.getElementById("bizztrak_img");
-bizztrak_img.src = `https://source.unsplash.com/R401qwThw7w/${window.innerWidth}x${window.innerHeight}`;
+// document.getElementById(
+//   "bizztrak"
+// ).style.backgroundImage = `url("https://source.unsplash.com/random/${self.offsetWidth}x${self.offsetHeight}")`;
 
-const party_img = document.getElementById("party_img");
-party_img.src = `https://source.unsplash.com/48elRjHbXRY/${window.innerWidth}x${window.innerHeight}`;
+// document.getElementById(
+//   "PartyAffiliation"
+// ).style.backgroundImage = `url("https://source.unsplash.com/random/${self.offsetWidth}x${self.offsetHeight}")`;
