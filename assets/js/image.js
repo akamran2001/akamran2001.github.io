e9d347a3b398a0d981bea6a125750973312f32fe
Ahmed Kamran <akamran@colgate.edu>
2021-11-22 01:29:27 -0500

    put tile styles in one page and changed images

--- assets/js/image.js
+++ assets/js/image.js
@@ -6,16 +6,10 @@ const isyllabi_img = document.getElementById("isyllabi_img");
 isyllabi_img.src = `https://source.unsplash.com/NIpx9ZUSiZg/${window.innerWidth}x${window.innerHeight}`;
 
 const clock_img = document.getElementById("clock_img");
-clock_img.src = `https://source.unsplash.com/JsTmUnHdVYQ/${window.innerWidth}x${window.innerHeight}`;
+clock_img.src = `https://source.unsplash.com/${window.innerWidth}x${window.innerHeight}/daily?scenic`;
 
 const saiyancity_img = document.getElementById("saiyancity_img");
-saiyancity_img.src = `https://source.unsplash.com/-98jVaVuGv0/${window.innerWidth}x${window.innerHeight}`;
-
-const bizztrak_img = document.getElementById("bizztrak_img");
-bizztrak_img.src = `https://source.unsplash.com/R401qwThw7w/${window.innerWidth}x${window.innerHeight}`;
-
-const party_img = document.getElementById("party_img");
-party_img.src = `https://source.unsplash.com/48elRjHbXRY/${window.innerWidth}x${window.innerHeight}`;
+saiyancity_img.src = `https://akamran2001.github.io/saiyancity/kh.png`;
 
 const cdf_img = document.getElementById("cdf_img");
 cdf_img.src = `https://i.ytimg.com/vi_webp/qdiD78cZALY/maxresdefault.webp`;
