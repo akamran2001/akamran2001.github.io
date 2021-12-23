d403c0a6a0228afe6c073672cf4c2ba82ac1f10e
Ahmed Kamran <akamran@colgate.edu>
2021-12-23 02:06:47 -0500

    robinhoodvis added

--- assets/js/image.js
+++ assets/js/image.js
@@ -13,3 +13,6 @@ saiyancity_img.src = `https://akamran2001.github.io/saiyancity/kh.png`;
 
 const cdf_img = document.getElementById("cdf_img");
 cdf_img.src = `https://i.ytimg.com/vi_webp/qdiD78cZALY/maxresdefault.webp`;
+
+const robinhood_img = document.getElementById("robinhood_img");
+robinhood_img.src = `https://images.unsplash.com/photo-1549421263-5ec394a5ad4c`;
